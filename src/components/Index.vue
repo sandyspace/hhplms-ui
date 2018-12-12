21ad71e8297411e5d50b0c6232ede839e6969846
Jason.Ma <bulletmarker@qq.com>
2018-12-12 18:25:36 +0800

    Index change

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -5,7 +5,9 @@
     <div><router-link to="/1818">1818中心</router-link></div>
     <div><router-link to="/cbgj">船舶国际广场</router-link></div>
     <div><router-link to="/xmgk">项目概况</router-link></div>
+    <div><router-link to="/register">用户注册</router-link></div>
     <div><router-link to="/login">登录</router-link></div>
+    <div><router-link to="/user">用户中心</router-link></div>
   </div>
 </template>
 
